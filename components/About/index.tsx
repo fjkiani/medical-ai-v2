a63969cad8c7161b6e66390e7bda82b58183d18a
Fahad Kiani <fjkiani@icloud.com>
2024-07-19 12:52:33 -0400

    changes

---
+++ components/About/index.tsx
@@ -0,0 +1,218 @@
+"use client";
+
+import { motion } from "framer-motion";
+import Image from "next/image";
+
+const About = () => {
+  return (
+    <>
+      {/* <!-- ===== About Start ===== --> */}
+      <section className="overflow-hidden pb-20 lg:pb-25 xl:pb-30">
+        <div className="mx-auto max-w-c-1235 px-4 md:px-8 xl:px-0">
+          <div className="flex items-center gap-8 lg:gap-32.5">
+            <motion.div
+              variants={{
+                hidden: {
+                  opacity: 0,
+                  x: -20,
+                },
+
+                visible: {
+                  opacity: 1,
+                  x: 0,
+                },
+              }}
+              initial="hidden"
+              whileInView="visible"
+              transition={{ duration: 0.5, delay: 0.1 }}
+              viewport={{ once: true }}
+              className="animate_left relative mx-auto hidden aspect-[588/526.5] md:block md:w-1/2"
+            >
+              <Image
+                src="/images/about/about-light-01.png"
+                alt="About"
+                className="dark:hidden"
+                fill
+              />
+              <Image
+                src="/images/about/about-dark-01.png"
+                alt="About"
+                className="hidden dark:block"
+                fill
+              />
+            </motion.div>
+            <motion.div
+              variants={{
+                hidden: {
+                  opacity: 0,
+                  x: 20,
+                },
+
+                visible: {
+                  opacity: 1,
+                  x: 0,
+                },
+              }}
+              initial="hidden"
+              whileInView="visible"
+              transition={{ duration: 0.5, delay: 0.1 }}
+              viewport={{ once: true }}
+              className="animate_right md:w-1/2"
+            >
+              <span className="font-medium uppercase text-black dark:text-white">
+                <span className="mb-4 mr-4 inline-flex rounded-full bg-meta px-4.5 py-1 text-metatitle uppercase text-white ">
+                  ->
+                </span>{" "}
+                Empower your diagnostic workflow 20x with AI 
+              </span>
+              <h2 className="relative mb-6 text-3xl font-bold text-black dark:text-white xl:text-hero">
+              AI-Powered X-Ray Analysis Platform
+                {/* <span className="relative inline-block before:absolute before:bottom-2.5 before:left-0 before:-z-1 before:h-3 before:w-full before:bg-titlebg dark:before:bg-titlebgdark">
+                  SaaS Startup
+                </span> */}
+              </h2>
+              <p>
+              Through deep learning techniques, our AI extracts intricate features from X-ray images, identifying subtle patterns that might escape the human eye. This capability is crucial for early detection of conditions like lung nodules, pneumonia, and cardiomegaly
+              </p>
+
+              <div className="mt-7.5 flex items-center gap-5">
+                <div className="flex h-15 w-15 items-center justify-center rounded-[50%] border border-stroke dark:border-strokedark dark:bg-blacksection">
+                  <p className="text-metatitle2 font-semibold text-black dark:text-white">
+                    01
+                  </p>
+                </div>
+                <div className="w-3/4">
+                  <h3 className="mb-0.5 text-metatitle2 text-black dark:text-white">
+                  X-ray Image Upload
+                  </h3>
+                  <p> This can be done directly from the hospital's PACS (Picture Archiving and Communication System) or through a web interface. Our platform supports various image formats and ensures that all data is encrypted during transmission to maintain patient confidentiality.</p>
+                </div>
+              </div>
+              <div className="mt-7.5 flex items-center gap-5">
+                <div className="flex h-15 w-15 items-center justify-center rounded-[50%] border border-stroke dark:border-strokedark dark:bg-blacksection">
+                  <p className="text-metatitle2 font-semibold text-black dark:text-white">
+                    02
+                  </p>
+                </div>
+                <div className="w-3/4">
+                  <h3 className="mb-0.5 text-metatitle2 text-black dark:text-white">
+                  AI-Powered Image Analysis
+                  </h3>
+                  <p>Once uploaded, our advanced AI algorithms begin analyzing the X-ray images. Using state-of-the-art computer vision techniques, the system processes the images to detect and highlight potential abnormalities.</p>
+                </div>
+                
+              </div>
+              <div className="mt-7.5 flex items-center gap-5">
+                <div className="flex h-15 w-15 items-center justify-center rounded-[50%] border border-stroke dark:border-strokedark dark:bg-blacksection">
+                  <p className="text-metatitle2 font-semibold text-black dark:text-white">
+                    03
+                  </p>
+                </div>
+                <div className="w-3/4">
+                  <h3 className="mb-0.5 text-metatitle2 text-black dark:text-white">
+                  Identification and Highlighting
+                  </h3>
+                  <p>The system identifies areas of concern and generates visual annotations directly on the X-ray images. These annotations highlight regions that require further attention, providing radiologists with a clear, visual representation of potential issues. The AI also assigns probability scores to each detected abnormality, indicating the likelihood of various conditions.</p>
+                </div>
+                
+              </div>
+            </motion.div>
+          </div>
+        </div>
+      </section>
+      {/* <!-- ===== About End ===== --> */}
+
+      {/* <!-- ===== About Two Start ===== --> */}
+      <section>
+        <div className="mx-auto max-w-c-1235 overflow-hidden px-4 md:px-8 2xl:px-0">
+          <div className="flex items-center gap-8 lg:gap-32.5">
+            <motion.div
+              variants={{
+                hidden: {
+                  opacity: 0,
+                  x: -20,
+                },
+
+                visible: {
+                  opacity: 1,
+                  x: 0,
+                },
+              }}
+              initial="hidden"
+              whileInView="visible"
+              transition={{ duration: 1, delay: 0.1 }}
+              viewport={{ once: true }}
+              className="animate_left md:w-1/2"
+            >
+              <h4 className="font-medium uppercase text-black dark:text-white">
+                Launch Your SaaS Fast
+              </h4>
+              <h2 className="relative mb-6 text-3xl font-bold text-black dark:text-white xl:text-hero">
+                Packed with All Essential {"   "}
+                <span className="relative inline-block before:absolute before:bottom-2.5 before:left-0 before:-z-1 before:h-3 before:w-full before:bg-titlebg2 dark:before:bg-titlebgdark">
+                  Integrations
+                </span>
+              </h2>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+                ultricies lacus non fermentum ultrices. Fusce consectetur le.
+              </p>
+              <div>
+                <a
+                  href="#"
+                  className="group mt-7.5 inline-flex items-center gap-2.5 text-black hover:text-primary dark:text-white dark:hover:text-primary"
+                >
+                  <span className="duration-300 group-hover:pr-2">
+                    Know More
+                  </span>
+                  <svg
+                    width="14"
+                    height="14"
+                    viewBox="0 0 14 14"
+                    fill="currentColor"
+                  >
+                    <path d="M10.4767 6.16701L6.00668 1.69701L7.18501 0.518677L13.6667 7.00034L7.18501 13.482L6.00668 12.3037L10.4767 7.83368H0.333344V6.16701H10.4767Z" />
+                  </svg>
+                </a>
+              </div>
+            </motion.div>
+            <motion.div
+              variants={{
+                hidden: {
+                  opacity: 0,
+                  x: 20,
+                },
+
+                visible: {
+                  opacity: 1,
+                  x: 0,
+                },
+              }}
+              initial="hidden"
+              whileInView="visible"
+              transition={{ duration: 1, delay: 0.1 }}
+              viewport={{ once: true }}
+              className="animate_right relative mx-auto hidden aspect-[588/526.5] md:block md:w-1/2"
+            >
+              <Image
+                src="./images/about/about-light-02.svg"
+                alt="About"
+                className="dark:hidden"
+                fill
+              />
+              <Image
+                src="./images/about/about-dark-02.svg"
+                alt="About"
+                className="hidden dark:block"
+                fill
+              />
+            </motion.div>
+          </div>
+        </div>
+      </section>
+      {/* <!-- ===== About Two End ===== --> */}
+    </>
+  );
+};
+
+export default About;
---
+++ components/Features/featuresData.tsx
@@ -0,0 +1,48 @@
+import { Feature } from "@/types/feature";
+
+const featuresData: Feature[] = [
+  {
+    id: 1,
+    icon: "/images/icon/icon-01.svg",
+    title: "Real-Time Analysis",
+    description:
+      "Instant results for faster diagnosis. Seamless API integration with existing PACS workflows",
+  },
+  {
+    id: 2,
+    icon: "/images/icon/icon-02.svg",
+    title: "Advanced Detection Algorithms",
+    description:
+      "Advanced deep learning algorithms for precise detection and continuous learning and improvement through radiologist feedback",
+  },
+  // {
+  //   id: 3,
+  //   icon: "/images/icon/icon-03.svg",
+  //   title: "Next.js 13 + TypeScript",
+  //   description:
+  //     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis tortor.",
+  // },
+  {
+    id: 4,
+    icon: "/images/icon/icon-04.svg",
+    title: "Secure and Compliant",
+    description:
+      "HIPAA-compliant data handling.",
+  },
+  // {
+  //   id: 5,
+  //   icon: "/images/icon/icon-05.svg",
+  //   title: "Intelligent Reporting",
+  //   description:
+  //     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis tortor.",
+  // },
+  // {
+  //   id: 6,
+  //   icon: "/images/icon/icon-06.svg",
+  //   title: "Regular Free Updates",
+  //   description:
+  //     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis tortor.",
+  // },
+];
+
+export default featuresData;
