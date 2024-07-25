004c06b4ddeb8173947d6be7c52cd31f7531d5b6
Fahad Kiani <fjkiani@icloud.com>
2024-07-25 18:24:17 -0400

    change

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -39,7 +39,7 @@ const Hero = () => {
                       aria-label="get started button"
                       className="flex rounded-full bg-black px-7.5 py-2.5 text-white duration-300 ease-in-out hover:bg-blackho dark:bg-btndark dark:hover:bg-blackho"
                     >
-                      Get Started
+                      Learn more
                     </button>
                   </div>
                 </form>
