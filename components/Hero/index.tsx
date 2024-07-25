da06821739d126d08da2763d3dd353108f060d12
Fahad Kiani <fjkiani@icloud.com>
2024-07-25 18:07:59 -0400

    changes

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -1,5 +1,4 @@
 "use client";
-import Image from "next/image";
 import { useState } from "react";
 
 const Hero = () => {
@@ -14,18 +13,16 @@ const Hero = () => {
       <section className="overflow-hidden pb-20 pt-35 md:pt-40 xl:pb-25 xl:pt-46">
         <div className="mx-auto max-w-c-1390 px-4 md:px-8 2xl:px-0">
           <div className="flex lg:items-center lg:gap-8 xl:gap-32.5">
-            <div className=" md:w-1/2">
-              {/* <h4 className="mb-4.5 text-lg font-medium text-black dark:text-white">
-              Fast track the diagnosis of suspected lung cancer patients in just 30 seconds
-              </h4> */}
-              <h1 className="mb-5 pr-16 text-3xl font-bold text-black dark:text-white xl:text-hero ">
-              Revolutionize X-Ray Diagnostics with  {"   "}
-                <span className="relative inline-block before:absolute before:bottom-2.5 before:left-0 before:-z-1 before:h-3 before:w-full before:bg-titlebg dark:before:bg-titlebgdark ">
-                AI
+            <div className="md:w-1/2">
+              <h1 className="mb-5 pr-16 text-3xl font-bold text-black dark:text-white xl:text-hero">
+                Revolutionize X-Ray Diagnostics with{" "}
+                <span className="relative inline-block before:absolute before:bottom-2.5 before:left-0 before:-z-1 before:h-3 before:w-full before:bg-titlebg dark:before:bg-titlebgdark">
+                  AI
                 </span>
               </h1>
               <p>
-              Fast track the diagnosis of suspected pneumonia patients in just 30 seconds utilizing AI-powered X-ray analysis platform
+                Fast track the diagnosis of suspected pneumonia patients in just
+                30 seconds utilizing AI-powered X-ray analysis platform
               </p>
 
               <div className="mt-10">
@@ -46,48 +43,23 @@ const Hero = () => {
                     </button>
                   </div>
                 </form>
-
-                {/* <p className="mt-5 text-black dark:text-white">
-                  Try for free no credit card required.
-                </p> */}
               </div>
             </div>
 
             <div className="animate_right hidden md:w-1/2 lg:block">
               <div className="relative 2xl:-mr-7.5">
-                <Image
-                  src="/images/shape/shape-01.png"
-                  alt="shape"
-                  width={46}
-                  height={246}
-                  className="absolute -left-11.5 top-0"
-                />
-                <Image
-                  src="/images/shape/shape-02.svg"
-                  alt="shape"
-                  width={36.9}
-                  height={36.7}
-                  className="absolute bottom-0 right-0 z-10"
-                />
-                <Image
-                  src="/images/shape/shape-03.svg"
-                  alt="shape"
-                  width={21.64}
-                  height={21.66}
-                  className="absolute -right-6.5 bottom-0 z-1"
-                />
-                <div className=" relative aspect-[700/444] w-full">
-                  <Image
+                <div className="relative aspect-[700/444] w-full">
+                  <video
                     className="shadow-solid-l dark:hidden"
-                    src="/images/hero/hero.jpg"
-                    alt="Hero"
-                    fill
+                    src="/images/hero/medical-ai.mp4"
+                    alt="Medical AI Video"
+                    controls
                   />
-                  <Image
+                  <video
                     className="hidden shadow-solid-l dark:block"
-                    src="/images/hero/hero.jpg"
-                    alt="Hero"
-                    fill
+                    src="/images/hero/medical-ai.mp4"
+                    alt="Medical AI Video"
+                    controls
                   />
                 </div>
               </div>
